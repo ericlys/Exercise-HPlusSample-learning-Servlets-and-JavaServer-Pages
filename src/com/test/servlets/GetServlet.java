c6bc43e0472bc29859283b35c4b2db998a6e5d12
Ériclys <ericlysm@gmail.com>
2018-07-04 21:55:51 -0300

    APIS for Servlet life Cycle init, service, destroy

--- src/com/test/servlets/GetServlet.java
+++ src/com/test/servlets/GetServlet.java
@@ -1,6 +1,7 @@
 package com.test.servlets;
 
 import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebInitParam;
 import javax.servlet.annotation.WebServlet;
@@ -10,13 +11,16 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns = "/getServlet", initParams = @WebInitParam(name ="URL", value = "http://weatherservice.com"))
+//@WebServlet(urlPatterns = "/getServlet", initParams = @WebInitParam(name ="URL", value = "http://weatherservice.com"))
 public class GetServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		ServletConfig config = getServletConfig ();
 		System.out.println (config.getInitParameter ("URL") );
+        ServletContext context = getServletContext ();
+        System.out.println (context.getInitParameter ("dbURL") );
+
 		//String value = req.getParameter("name");
 		String htmlResponse = "<html><h3>Welcome to Servlets !</h3></html>";
 		PrintWriter writer = resp.getWriter();
